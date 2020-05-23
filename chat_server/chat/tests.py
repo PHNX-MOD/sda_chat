354013fb04ad1778644ec431832d3ca290727776
Basar Turgut <bturgut@twilio.com>
2020-05-23 20:52:50 +0300

    Add tests

--- chat_server/chat/tests.py
+++ chat_server/chat/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RoomTestCase(TestCase):
+    def test_room_happy_path(self):
+        response = self.client.get('/chat/room?user=basar')
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['user_name'], 'basar')
+
+    def test_room_missing_user(self):
+        response = self.client.get('/chat/room')
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 400)
